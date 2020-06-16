4d136e046c81685bf796146d3a163489faf5595a
EvaLantsova <55274804+EvaLantsova@users.noreply.github.com>
2020-06-17 01:42:29 +0300

    added manifesto page

---
+++ javascript/manifesto.js
@@ -0,0 +1,11 @@
+var manifesto_book_el = document.getElementById('manifesto_book');
+
+window.addEventListener('resize', function(e) {
+  manifesto_book_el.style.width = '';
+  manifesto_book_el.style.height = '';
+  $(manifesto_book_el).turn('size', manifesto_book_el.clientWidth, manifesto_book_el.clientHeight);
+});
+
+ $(manifesto_book_el).turn({
+ autoCenter: true
+})
